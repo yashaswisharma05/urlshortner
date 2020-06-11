3dc70bbb3f7df51049e294888fff9f6e1f31f06b
yashaswisharma05 <66776723+yashaswisharma05@users.noreply.github.com>
2020-06-11 15:17:35 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+def create_app(test_config=None):
+    app=Flask(__name__)
+    app.secret_key='12345abcedf'
+
+    from . import urlshort
+    app.register_blueprint(urlshort.bp)
+    return app
